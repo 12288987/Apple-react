f36164dca9cb5d96aa422311f422b5a66473a476
Ebrahim Essa Ahmed <99309335+12288987@users.noreply.github.com>
2024-12-27 15:00:33 +0300

    header modified

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -4,28 +4,31 @@ import cart from "../images/icons/cart.png"
 
 function Header() {
   return (
-        <header className="backGR py-1">
-        <div className="container">
-            <ul className="row">
-            <li className="col text-center">
-                <a href="#"><img src={logo} alt="" /></a>
-            </li>
-            <li className="col d-none d-md-block"><a href="#">Mac</a></li>
-            <li className="col d-none d-md-block"><a href="#">iphone</a></li>
-            <li className="col d-none d-md-block"><a href="#">ipad</a></li>
-            <li className="col d-none d-md-block"><a href="#">watch</a></li>
-            <li className="col d-none d-md-block"><a href="#">tv</a></li>
-            <li className="col d-none d-md-block"><a href="#">Music</a></li>
-            <li className="col d-none d-md-block"><a href="#">Support</a></li>
-            <li className="col d-none d-md-block">
-                <a href="#"><img src={search} alt="" /></a>
-            </li>
-            <li className="col d-none d-md-block">
-                <a href="#"><img src={cart} alt="" /></a>
-            </li>
-            </ul>
-        </div>
-        </header>
+    <header className="backGR py-1">
+    <div className="container">
+      <ul className="row navv">
+        <li className="col-9 col-md aplogo">
+          <a href="#"><img src={logo} alt="" /></a>
+        </li>
+        <li className="col-md d-none d-md-block"><a href="#">Mac</a></li>
+        <li className="col-md d-none d-md-block"><a href="#">iphone</a></li>
+        <li className="col-md d-none d-md-block"><a href="#">ipad</a></li>
+        <li className="col-md d-none d-md-block"><a href="#">Watch</a></li>
+        <li className="col-md d-none d-md-block"><a href="#">Tv</a></li>
+        <li className="col-md d-none d-md-block"><a href="#">Music</a></li>
+        <li className="col-md d-none d-md-block"><a href="#">Support</a></li>
+        <li className="col-1 col-md d-md-block">
+          <a href="#"><img src={search} alt="" /></a>
+        </li>
+        <li className="col-1 col-md d-md-block">
+          <a href="#"><img src={cart} alt="" /></a>
+        </li>
+        <li className="col-1 col-md d-md-none fontas">
+          <i className="fa-solid fa-bars op" id="op"></i>
+        </li>
+      </ul>
+    </div>
+  </header>
   )
 }
 

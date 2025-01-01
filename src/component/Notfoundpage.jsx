18a8e91e110f456a94b6914fec436984bbd978c4
Ebrahim Essa Ahmed <99309335+12288987@users.noreply.github.com>
2025-01-01 16:31:32 +0300

    update the routing

---
+++ src/component/Notfoundpage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Notfoundpage() {
+  return (
+    <div className='text-center fw-bold'>
+        <br />
+        <br />
+        <p>404</p>
+        <p>Your page is not Found</p>
+    </div>
+  )
+}
+
+export default Notfoundpage;

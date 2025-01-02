731028eb74030fba52da176bd12ce80afce97061
Ebrahim Essa Ahmed <99309335+12288987@users.noreply.github.com>
2025-01-02 08:38:31 +0300

    connect my iphone page to my database

--- src/component/pages/iphone.jsx
+++ src/component/pages/iphone.jsx
@@ -1,14 +1,68 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
+import './Iphone.css'
 
 function Iphone() {
+  const [products, setProducts] = useState([]);
+  useEffect(()=>{
+    fetch("http://localhost:3030/productInfo3").then((res)=> res.json()).then((data)=>{
+      setProducts(data.products);
+    }).catch(()=>console.log("Error: unabel to fetch!!"));
+  }, []);
+
+  let flip = true;
+
   return (
-    <div className='text-center fw-bold'>
-        <br/>
-        <br/>
-        <p>
-        This is iphone page
-        </p>
-    </div>
+    <>
+      <section className='internal-page-wrapper'>
+        <div className='container'>
+          <div className='row justify-content-center text-center'>
+            <div className='col-12 mt-5 pt-5'>
+              <h1 className='font-weight-bold'>Iphones</h1>
+              <div className='brief-description mb-5'>
+                The best for the brightest.
+              </div>
+            </div>
+          </div>
+          {products?.map((product)=> {
+            let order1 = 1;
+            let order2 = 2;
+            if(flip) {
+              order1 = 2;
+              order2 = 1;
+              flip = !flip;
+            } else {
+              flip = !flip;
+            }
+
+            let productDiv = (
+                <div key = {product.product_id} className="row justify-content-center text-center product-holder h-100">
+                  <div className={`col-sm-12 col-md-6 my-4 my-md-auto order-md-${order1}`}>
+                      <div className='product-title'>{product.product_name}</div>
+                      <div className='product-brief'>{product.product_description}</div>
+                      <div className='price'>{product.product_price}</div>
+                      <div className='links-wrapper'>
+                        <ul>
+                          <li>
+                            <Link to={`/iphones/${product.product_url}`} className='lmore linkstyle'>
+                              Learn more
+                            </Link>
+                          </li>
+                        </ul>
+                      </div>
+                  </div>
+                  <div className={`col-sm-12 col-md-6 order-md-${order2}`}>
+                    <div className='product-image'>
+                      <img src={product.product_link} alt="product" />
+                    </div>
+                  </div>
+                </div>
+            );
+            return productDiv;
+          })}
+        </div>
+      </section>  
+    </>
   )
 }
 
